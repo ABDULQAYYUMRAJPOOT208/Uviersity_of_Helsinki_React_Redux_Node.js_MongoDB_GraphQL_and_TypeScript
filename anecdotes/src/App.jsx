c370ec36b738108e60067e760ec0ed742900cc3a
ABDULQAYYUMRAJPOOT208 <156652998+ABDULQAYYUMRAJPOOT208@users.noreply.github.com>
2024-08-01 07:29:40 -0700

    ex_1.12

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -11,19 +11,30 @@ const App = () => {
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
     "The only way to go fast, is to go well.",
   ];
-
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
   const [selected, setSelected] = useState(0);
 
   return (
     <div>
       {anecdotes[selected]}
-      <br />
+      <p>Has {votes[selected]} Votes</p>
       <button
-        onClick={() =>
-          setSelected(Math.floor(Math.random() * anecdotes.length))
-        }
+        onClick={(e) => {
+          e.preventDefault();
+          setSelected(Math.floor(Math.random() * anecdotes.length));
+        }}
       >
         Next Anecdote
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          const newVotes = [...votes];
+          newVotes[selected]++;
+          setVotes(newVotes);
+        }}
+      >
+        Vote
       </button>
     </div>
   );
