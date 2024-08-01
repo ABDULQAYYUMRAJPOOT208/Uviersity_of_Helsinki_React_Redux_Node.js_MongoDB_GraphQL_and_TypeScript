6c773bab915b657a5677f97fa5c35a6263c31b70
ABDULQAYYUMRAJPOOT208 <156652998+ABDULQAYYUMRAJPOOT208@users.noreply.github.com>
2024-08-01 06:49:58 -0700

    ex_1.6-1.11

--- Unicafe/src/App.jsx
+++ Unicafe/src/App.jsx
@@ -1,8 +1,25 @@
 import Header from "../components/Header";
 import Content from "../components/Content";
 import Total from "../components/Total";
+import { useState } from "react";
+import Statics from "../components/Statics";
 const App = () => {
-  const course = "Half Stack application development";
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const handleGood = (e) => {
+    e.preventDefault();
+    setGood(good + 1);
+  };
+  const handleNeutral = (e) => {
+    e.preventDefault();
+    setNeutral(neutral + 1);
+  };
+  const handleBad = (e) => {
+    e.preventDefault();
+    setBad(bad + 1);
+  };
+  const course = "Give FeedBack";
   const parts = [
     {
       name: "Fundamentals of React",
@@ -20,8 +37,17 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      {/* <Content parts={parts} />
+      <Total parts={parts} /> */}
+      <div>
+        <button onClick={handleGood}>good</button>
+        <button onClick={handleNeutral}>neutral</button>
+        <button onClick={handleBad}>bad</button>
+      </div>
+      <br />
+      <h2>statistics</h2>
+      <br />
+      <Statics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
